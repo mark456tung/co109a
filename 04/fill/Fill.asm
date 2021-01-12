33fb4ba12d9618c44aefea2c33a182fcfd2412c5
mark456tung <mark456tung@gmail.com>
2021-01-12 21:00:16 +0800

    add homework

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,36 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//16384---->screen
+//24576---->keyboard
+@8192               
+D=A               
+@0                
+M=D                
+@24576
+D=M
+@18                  
+D;JNE            //IF OUT!=0 JUMP TO LINE 18
+//WHITE LOOP
+@0
+D=M               
+M=M-1                
+@0              
+D;JEQ           //IF OUT ==0 JUMP TO 0     
+@16383          //16384開始是螢幕
+A=D+A                
+M=0              
+@8                
+0;JMP         //開始下次迴圈       
+//BLACK LOOP
+@0
+D=M
+M=M-1
+@0
+D;JEQ        //IF OUT ==0 JUMP TO 0        
+@24576
+A=A-D
+M=-1
+@18
+0;JMP       //開始下次迴圈
